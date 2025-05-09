403c35e6795d494bf1f45560d2b01daa696a1507
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-09 13:43:02 -0400

    updated skills cards

--- src/Components/SkillsSection/SkillsSection.jsx
+++ src/Components/SkillsSection/SkillsSection.jsx
@@ -14,97 +14,97 @@ function SkillsSection() {
                     <div className="skill">
                         <img src="/assets/python.jpg" alt="Python logo" />
                         <h6>Python</h6>
-                        <p>Duration?</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/flask.png" alt="Flask logo" />
                         <h6>Flask</h6>
-                        <p>Duration?</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/Django.jpg" alt="Django logo" />
                         <h6>Django</h6>
-                        <p>Duration?</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/Docker.png" alt="Docker logo" />
                         <h6>Docker</h6>
-                        <p>duration</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/Kubernetes.jpg" alt="Kubernetes logo" />
                         <h6>Kubernetes</h6>
-                        <p>duration</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/Openshift.jpg" alt="Openshift logo" />
                         <h6>Openshift</h6>
-                        <p>duration</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/MongoDB.jpg" alt="MognoDB logo" />
                         <h6>MongoDB</h6>
-                        <p>duration</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/Tekton.png" alt="Tekton logo" />
                         <h6>Tekton</h6>
-                        <p>duration</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/IBM%20Cloud.jpg" alt="IBM Cloud logo" />
                         <h6>IBM Cloud</h6>
-                        <p>duration</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/GitHub.png" alt="GitHub logo" />
                         <h6>GitHub</h6>
-                        <p>duration</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/react.png" alt="react logo" />
                         <h6>React</h6>
-                        <p>1 month</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/Vite.png" alt="Vite logo" />
                         <h6>Vite</h6>
-                        <p>duration?</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/Bootstrap.png" alt="Bootstrap logo" />
                         <h6>Bootstrap</h6>
-                        <p>duration?</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/html5.png" alt="html logo" />
                         <h6>HTML</h6>
-                        <p>4 months</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/CSS3.png" alt="css logo" />
                         <h6>CSS</h6>
-                        <p>4 months</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
 
                     <div className="skill">
                         <img src="/assets/js.jpeg" alt="javascript logo" />
                         <h6>JavaScript</h6>
-                        <p>4 months</p>
+                        <div>1 yrs, 5 mts</div>
                     </div>
                 </div>
             </section>
