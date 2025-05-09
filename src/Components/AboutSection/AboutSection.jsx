0ec51cbd0084b48e0cdb9e219ed660b2521f51ca
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-08 23:40:17 -0400

    updated css

--- src/Components/AboutSection/AboutSection.jsx
+++ src/Components/AboutSection/AboutSection.jsx
@@ -3,22 +3,24 @@ import React from "react";
 
 function AboutSection() {
     return (
-        <div className="AboutSection">
-            {/* About Me */}
-            <section>
-                <h2>About</h2>
-                <div style={{ clear: 'both' }}></div>
-                <p className="main-p">
-                    I'm searching for an entry-level position in Software Engineering.
-                    I currently have experience in creating front-end applications using React with Vite,
-                    Express apps with Node.js, Python apps with Flask & MongoDB, and full-stack apps with Django.
-                    I've also learned how to create unit tests in Python with unittests, and JavaScript with Jasmine,
-                    for test-driven and behavior-driven development. I'm familiar with containerization using Docker,
-                    Kubernetes, & OpenShift, as well as building CI/CD pipelines.
-                    I also have an understanding of Agile & how to practice DevOps.
-                </p>
-            </section>
-        </div>
+        <>
+            <div className="AboutSection">
+                {/* About Me */}
+                <section>
+                    <h2>About</h2>
+                    <div style={{ clear: 'both' }}></div>
+                    <p className="main-p">
+                        I'm searching for an entry-level position in Software Engineering.
+                        I currently have experience in creating front-end applications using React with Vite,
+                        Express apps with Node.js, Python apps with Flask & MongoDB, and full-stack apps with Django.
+                        I've also learned how to create unit tests in Python with unittests, and JavaScript with Jasmine,
+                        for test-driven and behavior-driven development. I'm familiar with containerization using Docker,
+                        Kubernetes, & OpenShift, as well as building CI/CD pipelines.
+                        I also have an understanding of Agile & how to practice DevOps.
+                    </p>
+                </section>
+            </div>
+        </>
     )
 }
 
