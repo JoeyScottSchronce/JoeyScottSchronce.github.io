db1c69bf55d155af09cd087f9e3d1ac407095864
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-12 21:17:46 -0400

    update css

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import "./HeroSection.css"
+import {Link} from "react-router-dom";
 
 function HeroSection() {
     return (
         <div id="HeroSection" className="container">
+            <div className="sub-container">
+                <div className="first-sub">
+                    <h1>Hero Section Title as an Intro</h1>
+                </div>
 
-            <div className="first-sub">
-                <h1>Hero Section Title as an Intro</h1>
+                <div className="second-sub">
+                    <h2>Second statement to maintain attention </h2>
+                </div>
+
+                <div className="third-sub" >
+                    <Link to="/Projects">Go to Projects</Link>
+                </div>
             </div>
-
-            <div className="second-sub">
-                <h2>Second statement to maintain attention </h2>
-            </div>
-
         </div>
     )
 }
