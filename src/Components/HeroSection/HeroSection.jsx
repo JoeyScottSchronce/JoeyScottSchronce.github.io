a72b9a5aee0cb1f18c05ee49b6406c0110332bd1
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-12 20:45:17 -0400

    updated css

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -4,12 +4,15 @@ import "./HeroSection.css"
 function HeroSection() {
     return (
         <div id="HeroSection" className="container">
+
             <div className="first-sub">
-
+                <h1>Hero Section Title as an Intro</h1>
             </div>
+
             <div className="second-sub">
-
+                <h2>Second statement to maintain attention </h2>
             </div>
+
         </div>
     )
 }
