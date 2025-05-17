fe850ff858a4c60e9a2dda012158ae0cb8618174
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-17 13:42:05 -0400

    updated css

--- src/Components/Credentials/Credentials.jsx
+++ src/Components/Credentials/Credentials.jsx
@@ -8,22 +8,19 @@ function Credentials() {
         <div className="credentials-main-container">
             <div className="credentials-sub-container">
                 <Link to="/certificates" className="link-sub-container">
-                    <img src="https://www.centroarbitrajeconciliacion.com/var/ccb/storage/images/media/cac/ejemplos-de-contenido/kitchen-sink-lavaplatos/1283-40-esl-CO/Kitchen-sink-Lavaplatos.png" className="button-image" alt="" />
-                    <h3>Certificates</h3>
+                    <h1>Certificates</h1>
                 </Link>
             </div>
 
             <div className="credentials-sub-container">
                 <Link to="/badges" className="link-sub-container">
-                    <img src="https://www.centroarbitrajeconciliacion.com/var/ccb/storage/images/media/cac/ejemplos-de-contenido/kitchen-sink-lavaplatos/1283-40-esl-CO/Kitchen-sink-Lavaplatos.png" className="button-image" alt="" />
-                    <h3>Badges</h3>
+                    <h1>Badges</h1>
                 </Link>
             </div>
 
             <div className="credentials-sub-container">
                 <Link to="/skills" className="link-sub-container">
-                    <img src="https://www.centroarbitrajeconciliacion.com/var/ccb/storage/images/media/cac/ejemplos-de-contenido/kitchen-sink-lavaplatos/1283-40-esl-CO/Kitchen-sink-Lavaplatos.png" className="button-image" alt="" />
-                    <h3>Skills</h3>
+                    <h1>Skills</h1>
                 </Link>
             </div>
 
