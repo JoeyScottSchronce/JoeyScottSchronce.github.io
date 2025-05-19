aa9d3cde38f196c58caa41a1e5370c60b0990bb8
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-19 05:50:10 -0400

    updated badge data

--- src/Components/Credentials/Badges/BadgesData.js
+++ src/Components/Credentials/Badges/BadgesData.js
@@ -1,187 +1,187 @@
 const BadgesData = {
     1: {
         badge: "https://www.credly.com/badges/a8d2d639-6b18-4af9-a836-5f083b442a5c/public_url",
-        image: ""
+        image: "/assets/badges/DevOps_badge.webp"
     },
     2: {
         badge: "https://www.credly.com/badges/e806232e-be45-478c-906f-4c89aba8f052/public_url",
-        image: ""
+        image: "/assets/badges/backend_badge.webp"
     },
     3: {
         badge: "https://www.credly.com/badges/f6c0efc7-667f-4dc1-8ad0-163eba99ee71/public_url",
-        image: ""
+        image: "/assets/badges/frontend_badge.webp"
     },
     4: {
         badge: "https://www.credly.com/badges/d147fa15-33f1-46de-8216-91e822c6a449/public_url",
-        image: ""
+        image: "/assets/badges/soft_skills_badge.webp"
     },
     5: {
         badge: "https://www.credly.com/badges/1abcbc20-ee6a-4968-b85a-7262fbf4eb4b/public_url",
-        image: ""
+        image: "/assets/badges/DevOps_capstone.webp"
     },
     6: {
         badge: "https://www.credly.com/badges/7d4e5c33-68a3-4b1c-acf9-e70b009a0470/public_url",
-        image: ""
+        image: "/assets/badges/backend_capstone.webp"
     },
     7: {
         badge: "https://www.credly.com/badges/ec6ef621-2db1-4e0f-b6fd-5033c4aa1d92/public_url",
-        image: ""
+        image: "/assets/badges/frontend_capstone.webp"
     },
     8: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     9: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     10: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     11: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     12: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     13: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     14: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     15: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     16: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     17: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     18: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     19: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     20: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     21: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     22: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     23: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     24: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     25: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     26: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     27: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     28: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     29: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     30: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     31: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     32: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     33: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     34: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     35: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     36: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     37: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     38: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     39: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     40: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     41: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     42: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     43: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     44: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     45: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     },
     46: {
-        badge: "",
-        image: ""
+        badge: "https://www.credly.com/badges/",
+        image: "/assets/badges/"
     }
 }
 
