b0287eecc3e3e217bbb0e4602d6c3ec658905cdb
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-22 01:59:59 -0400

    updated css

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
     output: 'export', // Enables static export
     trailingSlash: true, // Adds trailing slashes to URLs (recommended for GitHub Pages)
+    basePath: '',
+    assetPrefix: '',
 };
 
 export default nextConfig;
