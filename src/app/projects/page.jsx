22071a4d8e92b7451805006d4b1d795fbbc8d385
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-22 01:19:29 -0400

    updated css

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,26 +1,26 @@
 
-import "./ProjectSection.css";
+import styles from "./projects.module.css";
 import ProjectCardData from "./ProjectCardData.js";
 import React from "react";
 
 function ProjectCard({ title, image, content, website, github }) {
 
     return (
-        <div className="slide-box">
-            <div className="card-container">
-                <h3 className={image ? "card-title" : "large-title"}>{title}</h3>
-                <div className="card-content-container">
+        <div className={styles.slideBox}>
+            <div className={styles.cardContainer}>
+                <h3 className={image ? styles.cardTitle : styles.largeTitle}>{title}</h3>
+                <div className={styles.cardContentContainer}>
                     {image && (
-                        <div className="card-image" ><img src={image} alt={title}/></div>
+                        <div className={styles.cardImage} ><img src={image} alt={title}/></div>
                     )}
                     <div>
-                        <div className={image ? "card-content" : "large-content"}>
-                            <h6 className="card-description">{content}</h6>
+                        <div className={image ? styles.cardContent : styles.largeContent}>
+                            <h6 className={styles.cardDescription}>{content}</h6>
                         </div>
-                        <div className="card-button-container">
-                            <a href={github}><button className="card-button">Project Repository</button></a>
+                        <div className={styles.cardButtonContainer}>
+                            <a href={github}><button className={styles.cardButton}>Project Repository</button></a>
                             {website && (
-                                <a href={website}><button className="card-button">See the Website</button></a>
+                                <a href={website}><button className={styles.cardButton}>See the Website</button></a>
                             )}
                         </div>
                     </div>
@@ -33,16 +33,16 @@ function ProjectCard({ title, image, content, website, github }) {
 const ProjectSection = () => {
 
     return (
-        <div className="projects-container">
-            <div className="projects-title-container">
+        <div className={styles.projectsContainer}>
+            <div className={styles.projectsTitleContainer}>
                 <h2>Projects</h2>
             </div>
-            <div className="projects-sub-container">
+            <div className={styles.projectsSubContainer}>
                 {Object.values(ProjectCardData).map((project, index) => (
                     <ProjectCard key={index} {...project} />
                 ))}
             </div>
-            <h6 className="directions-header">Swipe right to see more projects</h6>
+            <h6 className={styles.directionsHeader}>Swipe right to see more projects</h6>
         </div>
     );
 };
