e70145dcfe98db5bc31f18c1072adeee62795c97
JoeyScottSchronce <joeyscottschronce@gmail.com>
2025-05-23 01:35:19 -0400

    updated css

--- src/Components/ProjectSection/ProjectCard.jsx
+++ src/Components/ProjectSection/ProjectCard.jsx
@@ -4,26 +4,26 @@ import "./ProjectCard.css"
 function ProjectCard({ title, image, content, website, github }) {
 
     return (
-        <div className="card-container">
-            <h3 className={image ? "card-title" : "large-title"}>{title}</h3>
-            <div className="card-content-container">
-                {image && (
-                    <div className="card-image" ><img src={image} alt={title}/></div>
-                )}
-                <div>
-                    <div className={image ? "card-content" : "large-content"}>
-                        <h6 className="card-description">{content}</h6>
-                    </div>
-                    <div className="card-button-container">
-                        <a href={github}><button className="card-button">Project Repository</button></a>
-                        {website && (
-                            <a href={website}><button className="card-button">See the Website</button></a>
-                        )}
+        <div className="slide-box">
+            <div className="card-container">
+                <h3 className={image ? "card-title" : "large-title"}>{title}</h3>
+                <div className="card-content-container">
+                    {image && (
+                        <div className="card-image" ><img src={image} alt={title}/></div>
+                    )}
+                    <div>
+                        <div className={image ? "card-content" : "large-content"}>
+                            <h6 className="card-description">{content}</h6>
+                        </div>
+                        <div className="card-button-container">
+                            <a href={github}><button className="card-button">Project Repository</button></a>
+                            {website && (
+                                <a href={website}><button className="card-button">See the Website</button></a>
+                            )}
+                        </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
     )
 }
--- src/Components/ProjectSection/ProjectSection.jsx
+++ src/Components/ProjectSection/ProjectSection.jsx
@@ -4,38 +4,17 @@ import "./ProjectSection.css";
 import ProjectCardData from "./ProjectCardData.js";
 
 const ProjectSection = () => {
-    const projectEntries = Object.entries(ProjectCardData);
-    const [startIndex, setStartIndex] = useState(0);
-
-    // Number of projects displayed at a time
-    const DISPLAY_COUNT = 1;
-
-    // Next & Back button handlers
-    const handleNext = () => {
-        if (startIndex + DISPLAY_COUNT < projectEntries.length) {
-            setStartIndex(startIndex + DISPLAY_COUNT);
-        }
-    };
-
-    const handleBack = () => {
-        if (startIndex - DISPLAY_COUNT >= 0) {
-            setStartIndex(startIndex - DISPLAY_COUNT);
-        }
-    };
 
     return (
         <div className="projects-container">
             <div className="projects-title-container">
                 <h2>Projects</h2>
             </div>
+            <h6 className="directions-header">Swipe right to see more projects</h6>
             <div className="projects-sub-container">
-                {projectEntries.slice(startIndex, startIndex + DISPLAY_COUNT).map(([key, project]) => (
-                    <ProjectCard key={key} {...project} />
+                {Object.values(ProjectCardData).map((project, index) => (
+                    <ProjectCard key={index} {...project} />
                 ))}
-                <div className="project-nav-buttons">
-                    <button className="project-button" onClick={handleBack} disabled={startIndex === 0}>Back</button>
-                    <button className="project-button" onClick={handleNext} disabled={startIndex + DISPLAY_COUNT >= projectEntries.length}>Next</button>
-                </div>
             </div>
         </div>
     );
